a5be05c55183a92914a045b6efdc25a2f0e78ef7
Lelalu04 <ricardascep@gmail.com>
2022-10-24 22:53:57 +0300

    update text colors, search add

--- script.js
+++ script.js
@@ -22,30 +22,68 @@ form.addEventListener(`click`, (e) => {
     e.preventDefault()
     fetch(`https://api.chucknorris.io/jokes/categories`)
         .then(res => res.json())
-        .then(joke => {
-            joke.map(element => {
+        .then(categories => {
+            categories.map(category => {
                 let option = document.createElement(`option`)
-                option.value = element
-                option.textContent = element
+                option.value = category
+                option.textContent = category
                 select.append(option)
             })
         })
 
-    })
-    updarteJokeFronCategory.addEventListener(`click`, () => {
-        fetch(`https://api.chucknorris.io/jokes/random?category=${select.value}`)
-    .then(res => res.json())
-    .then(joke => {
-        p.textContent = joke.value
-    })
-    })
+})
+updarteJokeFronCategory.addEventListener(`click`, () => {
+    fetch(`https://api.chucknorris.io/jokes/random?category=${select.value}`)
+        .then(res => res.json())
+        .then(joke => {
+            pText_Color(joke.value, `black`)
+        })
+})
 
-    randomJokeButton.addEventListener(`click`, () => {
+randomJokeButton.addEventListener(`click`, () => {
     fetch(`https://api.chucknorris.io/jokes/random`)
         .then(res => res.json())
         .then(joke => {
-            p.textContent = joke.value
-            console.log(`asd`)
+            pText_Color(joke.value, `black`)
         })
 })
+let searchForm = document.getElementById(`form-search-joke`)
+let searchInput = document.getElementById(`searchInput`)
+let jokeArr = []
+searchForm.addEventListener(`submit`, (e) => {
+    e.preventDefault()
+    fetch(`https://api.chucknorris.io/jokes/search?query=${searchInput.value}`)
+        .then(res => res.json())
+        .then(joke => {
+            if (joke.error) {
+                pText_Color(joke.error, `red`)
+            } else if (joke.total === 0) {
+                pText_Color(`Not found joke`, `red`)
+
+                // gfhh
+            } else {
+                if(select.value){
+                    joke.result.map(element => {
+                        if([...element.categories] == select.value){
+                            jokeArr.push(element.value)
+                        }
+                    })
+                    
+                }else{
+                    joke.result.map(element => {
+                    jokeArr.push(element.value)
+                })
+                }
+                console.log(jokeArr)
+                let randomElement = jokeArr[Math.floor(Math.random() * jokeArr.length)]
+                pText_Color(randomElement, `black`)
+            }
+            jokeArr= []
+        })
+        // e.target.reset()
+})
+function pText_Color(text, color) {
+    p.textContent = text
+    p.style.color = color
+}
 
