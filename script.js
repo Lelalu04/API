19d06a8202329250fe8f69d5b7b026743e741703
Lelalu04 <ricardascep@gmail.com>
2022-10-24 15:03:43 +0300

    create select

---
+++ script.js
@@ -0,0 +1,51 @@
+// fetch(`https://api.chucknorris.io/jokes/random`)
+// .then(res => res.json())
+// .then(joke => {
+
+//     let p = document.getElementById(`paragraf`)
+//     let button = document.querySelector(`button`)
+//     console.log(joke.value)
+//     p.textContent = joke.value
+//     button.addEventListener(`click`, () => {
+//         window.location.reload()
+//     })
+// })
+
+let p = document.getElementById(`paragraf`)
+let buttonChangeJoke = document.getElementById(`change-joke-button`)
+let categoryButton = document.getElementById(`category-button`)
+let updarteJokeFronCategory = document.getElementById(`update-joke`)
+let form = document.getElementById(`form-category-joke`)
+let select = document.getElementById(`category-joke`)
+
+form.addEventListener(`click`, (e) => {
+    e.preventDefault()
+    fetch(`https://api.chucknorris.io/jokes/categories`)
+        .then(res => res.json())
+        .then(joke => {
+            joke.map(element => {
+                let option = document.createElement(`option`)
+                option.value = element
+                option.textContent = element
+                select.append(option)
+            })
+        })
+
+    })
+    updarteJokeFronCategory.addEventListener(`click`, () => {
+        fetch(`https://api.chucknorris.io/jokes/random?category=${select.value}`)
+    .then(res => res.json())
+    .then(joke => {
+        p.textContent = joke.value
+    })
+    })
+
+// buttonChangeJoke.addEventListener(`click`, () => {
+//     fetch(`https://api.chucknorris.io/jokes/random`)
+//         .then(res => res.json())
+//         .then(joke => {
+//             p.textContent = joke.value
+//             console.log(`asd`)
+//         })
+// })
+
